499a2be7e2c347e990180c4a31f6956848c4f168
kunwarshubhamsingh <150168579+ducatshubham@users.noreply.github.com>
2025-07-25 13:15:49 +0000

    conversion of

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,6 @@
+const account_id = 8083741098
+let account_email = "kunwar@gmail.com"
+var account_email_password = "75927"
+account_city = "gurugram"
+
+console.table([account_email, account_email_password,account_id])
---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,8 @@
+let score = "20"
+console.log(typeof(score));
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber))
+
+
+let ILoggedIn = "kunwar"
+let BooleanIsLoggedIn = Boolean(ILoggedIn)
